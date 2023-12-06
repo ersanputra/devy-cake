bb203d9296fd37f8941ee79beb38075bba5d4c6b
ersancakra <ersancakra@gmail.com>
2023-12-06 12:27:02 +0700

    push

--- services/userService.js
+++ services/userService.js
@@ -77,7 +77,7 @@ class UserService {
       async loginAdmin(email, password) {
         // Cari pengguna berdasarkan email
         const user = await User.findOne({
-          where: { email: email, active: true, role: "ADMIN" }
+          where: { email: email, role: "ADMIN" }
         });
     
         if (!user) {
